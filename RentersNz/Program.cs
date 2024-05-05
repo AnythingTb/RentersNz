564ddb7575cc163d80c2c9cf606f9cdfbe1946f0
AnythingTb <ac121712@avcol.school.nz>
2024-05-06 10:50:22 +1200

    Fixed User Roles Not allowing the porgram to load up

--- RentersNz/Program.cs
+++ RentersNz/Program.cs
@@ -7,35 +7,17 @@ var connectionString = builder.Configuration.GetConnectionString("RentersNzDbCon
 
 builder.Services.AddDbContext<RentersNzDbContext>(options => options.UseSqlServer(connectionString));
 
-// Configure ASP.NET Core Identity with default user and role classes
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
 })
-    .AddRoles<IdentityRole>() // Add role management
-    .AddEntityFrameworkStores<RentersNzDbContext>();
+    .AddEntityFrameworkStores<RentersNzDbContext>(); // Configure Entity Framework stores
 
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-
-// Create roles during application startup if they don't exist
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-    if (!await roleManager.RoleExistsAsync("Admin"))
-    {
-        await roleManager.CreateAsync(new IdentityRole("Admin"));
-    }
-
-    if (!await roleManager.RoleExistsAsync("RegularUser"))
-    {
-        await roleManager.CreateAsync(new IdentityRole("RegularUser"));
-    }
-}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
